b091f23ffe7d482100c43bbbcdf9ea4d4df7d0f4
radinmov <radinmovahedi1@gmail.com>
2023-12-20 23:45:55 +0330

    2.2.7

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -7,22 +7,30 @@ export default function Login() {
     <Style>
       <div className="fh-wrapper">
         <div className="wrapper w-60 h-96 m-auto">
-          <div className="wrapper-inner">
-            <h2>ورود از طریق از شماره تلفن همراه</h2>
-            <h4>
-              لطفا کشور خود را انتخاب کرده و شماره تلفن همراه خود را وارد کنید .
+          <div className="wrapper-inner ">
+            <h2 className="text-right mt-2">
+              ورود از طریق از شماره تلفن همراه
+            </h2>
+            <h4 className="text-right mt-2">
+              لطفا کشور خود را انتخاب کرده و شماره تلفن همراه خود را وارد کنید
             </h4>
-            <div className="input-wrapper">
-              <p>شماره تلفن همراه</p>
-              <div className="int_1">
+            <div className="input-wrapper mt-2">
+              <p className="text-right">شماره تلفن همراه</p>
+              <div className="int_1 flex  align-center mt-3 ">
                 +98 |
-                <input className="intput" placeholder="9***********" />
+                <input className="intput ml-2" placeholder="9***********" />
               </div>
-              <input className="intput" placeholder="رمز عبور " />
+              <div className="int_2 flex justify-center mt-11">
+                <input className="intput" placeholder="رمز عبور " />
+              </div>
             </div>
-            <button>ورود</button>
-            <h3>رمز عبور خود را فراموش کرده ام </h3>
-            <h3>ورود از طریق ایمیل</h3>
+            <div className="btn mt-2 justify-center flex">
+              <button>ورود</button>
+            </div>
+            <h3 className="text-center mt-2">
+              رمز عبور خود را فراموش کرده ام{" "}
+            </h3>
+            <h3 className="text-center mt-2">ورود از طریق ایمیل</h3>
           </div>
         </div>
       </div>
--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -1,35 +1,53 @@
 import styled from "styled-components";
+import { pallete } from "../../theme";
 
 export const Style = styled.div`
-height: 100vh;
-display: flex;
-justify-content: center;
-align-items: center;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-.wrapper {
-  background: #222327;
-  width: 400px;
-  height: 405px;
+  .wrapper {
+    background: #222327;
+    width: 500px;
+    height: 520px;
 
-  .wrapper-inner {
-    h4 {
-      color: gray;
-    }
+    .wrapper-inner {
+      width: 420px;
+      margin: 0 auto;
+      h4 {
+        color: gray;
+        text-align: right;
+      }
 
-    .input-wrapper {
-      .int_1 {
-        width: 337px;
-        height: 50px;
-        background: #37383e;
-        margin: 0 auto;
-
+      .input-wrapper {
+        .int_1 {
+          width: 337px;
+          height: 40px;
+          background: #37383e;
+          margin: 0 auto;
+          border-radius: ${pallete.defaultRadiusS};
+          input {
+            border: none;
+            outline: 0;
+            background: inherit;
+          }
+        }
         input {
           border: none;
           outline: 0;
-          background: inherit;
+          background: #37383e;
+          border-radius: ${pallete.defaultRadiusS};
+        }
+      }
+      .btn {
+        button {
+          width: 337px;
+          height: 40px;
+          background: #aaaaaa;
+          border-radius: ${pallete.defaultRadiusS};
         }
       }
     }
   }
-}
 `;
