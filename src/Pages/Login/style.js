b30cce71e27b1eb4fcd4229eb4024cfe0a269005
radinmov <radinmovahedi1@gmail.com>
2023-12-20 23:10:56 +0330

    2.2.6

--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -1,32 +1,35 @@
 import styled from "styled-components";
+
 export const Style = styled.div`
-height:100vh;
-.fh-wrapper {
-    .wrapper {
-        background:#222327;
-        width:400px;
-        height:405px;
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+
+.wrapper {
+  background: #222327;
+  width: 400px;
+  height: 405px;
+
+  .wrapper-inner {
+    h4 {
+      color: gray;
     }
-    .wrapper-inner {
-        h4 {
-            color:gray;
+
+    .input-wrapper {
+      .int_1 {
+        width: 337px;
+        height: 50px;
+        background: #37383e;
+        margin: 0 auto;
+
+        input {
+          border: none;
+          outline: 0;
+          background: inherit;
         }
-        .input-wrapper {
-            .int_1 {
-                width:337px;
-                height:50px;
-                background:#37383e;
-                margin:0 auto;
-            input {
-                border: none;
-                outline: 0;
-                background: inherit;
-            }
-              }
-            }
-        }
+      }
     }
+  }
 }
-
-
 `;
