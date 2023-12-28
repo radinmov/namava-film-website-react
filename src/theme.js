5be33759fa985faca4b71dff37381c31962fd76a
Radin Movahedi <125978983+radinmov@users.noreply.github.com>
2023-12-28 15:05:38 +0330

    2.3.3

--- src/theme.js
+++ src/theme.js
@@ -7,3 +7,20 @@ export const pallete = {
   whiteColor1: "#eddecb",
   defaultRadiusS:"7px"
 };
+export const size  ={
+  xs :"400px",
+  sm :"600px",
+  md:"900px",
+  lg :"1280px",
+  xl :"1440px",
+  xxl :"1920px",
+};
+export const device = {
+  xs :` (max-width:${size.xs})`,
+  sm :` (max-width:${size.sm})`,
+  md :` (max-width:${size.md})`,
+  lg :` (max-width:${size.lg})`,
+  xl :` (max-width:${size.xl})`,
+  xxl :` (max-width:${size.xxl})`,
+
+}
