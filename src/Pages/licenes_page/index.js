c25e5f8ac37bc1749b3687bdac93b94f39c8d433
radinmov <radinmovahedi1@gmail.com>
2024-01-01 20:27:39 +0330

    2.4.0

--- src/Pages/licenes_page/index.js
+++ src/Pages/licenes_page/index.js
@@ -5,6 +5,16 @@ export default function Licens() {
     return (
         <Style>
             <Header_2_licens />
+            <div className="container-2">
+                <div className="container_inner">
+                    <p>اشتراک خود را انتخاب کنید </p>
+                    <div className="licens_1">
+                        <div className="licens_1_inner">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Style>
     )
 }
--- src/Pages/licenes_page/style.js
+++ src/Pages/licenes_page/style.js
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const Style = styled.div``;
+export const Style = styled.div`
+background:#f2f2f2;
+height:100vh;
+marign-top:300px;
+`;
