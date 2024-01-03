e0b5b8abaf5b58f2f36a11c5c7d6f260c8ffc793
radinmov <radinmovahedi1@gmail.com>
2024-01-03 13:54:35 +0330

    2.4.4

--- src/Pages/licenes_page/index.js
+++ src/Pages/licenes_page/index.js
@@ -1,7 +1,9 @@
-import Header_2_licens from "../../componets/Header_licens"
-import { Style } from "./style"
+import Header_2_licens from "../../componets/Header_licens";
+import { Style } from "./style";
+import useTitle from "../../componets/Hook/useTitle";
 
 export default function Licens() {
+    const title = useTitle("خرید اشتراک نماوا ");
     return (
         <Style>
             <Header_2_licens />
@@ -9,8 +11,15 @@ export default function Licens() {
                 <div className="container_inner">
                     <p>اشتراک خود را انتخاب کنید </p>
                     <div className="licens_1">
-                        <div className="licens_1_inner">
-                            
+                        <div className="licens_1_inner flex ">
+                            <div className="right">
+                                <p>یک ماهه</p>
+                                <p className="bck">  ویژّه خرید اول %۳۰ تخفیف</p>
+                            </div>
+                            <div className="left">
+                                <span>۱۱۹,۰۰۰</span>
+                                <span>تومان</span>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/Pages/licenes_page/style.js
+++ src/Pages/licenes_page/style.js
@@ -4,4 +4,22 @@ export const Style = styled.div`
 background:#f2f2f2;
 height:100vh;
 marign-top:300px;
+.licens_1 {
+    width:680px;
+    height:108px;
+    background:#cecece;
+    color:#000;
+}
+.licens_1_inner {
+    width:665px;
+    height:108px;
+    background:#fff;
+}
+.bck {
+    width:180px;
+    color:#fff;
+    height:35px;
+    text-align:center;
+    background:#1993ff;
+}
 `;
