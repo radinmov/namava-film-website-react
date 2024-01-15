7de05317c1e326d747078c97d884d14b4769e9cb
Radin Movahedi <125978983+radinmov@users.noreply.github.com>
2024-01-15 19:48:39 +0330

    2.5.5

--- src/componets/Footer/style.js
+++ src/componets/Footer/style.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const Style = styled.div`
 background: #222327;
-// height:100px;
+position: fixed;
+bottom: 0;
+width:100%;
 .bootom-footer {
   background: #0d0d0d;
   padding: 10px 0;
